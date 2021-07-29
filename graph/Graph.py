345dd78894e282c0ca1e3bb4779195d5a3236c15
IsseNisse <isak.vindeln@gmail.com>
2021-07-29 18:49:28 +0200

    reworked method for checking if a node already exists

--- graph/Graph.py
+++ graph/Graph.py
@@ -11,29 +11,32 @@ edges = dict()
 i = 1
 j = 1
 
-
-def get_node_if_exists(node_to_check):
-    for node in nodes:
-        if node_to_check.node_number == nodes[node].node_number:
-            used_node = nodes[node]
-            return used_node
-        else:
-            return False
+#
+# def get_node_if_exists(node_to_check):
+#     for node in nodes:
+#         if node_to_check.node_number == nodes[node].node_number:
+#             used_node = nodes[node]
+#             return used_node
+#         else:
+#             return False
 
 
 for line in lines['edges']:
     node1 = Node(line['node1'])
     node2 = Node(line['node2'])
 
-    if get_node_if_exists(node1):
-        node1 = get_node_if_exists(node1)
-    else:
-        nodes[j] = node1
+    for node in nodes:
+        if node.node_number == node1.node_number:
+            from_node = node
+        if node.node_number == node2.node_number:
+            to_node = node
 
-    if get_node_if_exists(node2):
-        node2 = get_node_if_exists(node2)
-    else:
-        nodes[j + 1] = node2
+    if not from_node:
+        from_node = node1
+        nodes_len = len(nodes)
+        nodes[nodes_len + 1] = from_node
+    if not to_node:
+        to_node = node2
 
     edge = Edge(node1, node2, line['weight'])
     edges[i] = edge
