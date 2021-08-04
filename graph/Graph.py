28c44ac8a7e88149505c3c63b0b4bd57b93f0ee3
IsseNisse <isak.vindeln@gmail.com>
2021-08-05 01:07:57 +0200

    added the node and edge class to the graph to make it a module

--- graph/Graph.py
+++ graph/Graph.py
@@ -1,40 +1,57 @@
 import json
-from Edge import Edge
-from Node import Node
-
-graph_file = open("../graph1.json")
-lines = json.load(graph_file)
-
-nodes = dict()
-edges = dict()
-
-i = 1
 
 
-for line in lines['edges']:
-    node1 = Node(line['node1'])
-    node2 = Node(line['node2'])
-    from_node = None
-    to_node = None
+class Node:
+    def __init__(self, node_number):
+        self.node_number = node_number
 
-    for node_key in nodes:
-        if nodes[node_key].node_number == node1.node_number:
-            from_node = node_key
-        if nodes[node_key].node_number == node2.node_number:
-            to_node = node_key
 
-    if not from_node:
-        from_node = node1
-        nodes_len = len(nodes)
-        nodes[nodes_len + 1] = from_node
-    if not to_node:
-        to_node = node2
-        nodes_len = len(nodes)
-        nodes[nodes_len + 1] = to_node
+class Edge:
+    def __init__(self, nodeA, nodeB, weight):
+        self.node1 = nodeA
+        self.node2 = nodeB
+        self.weight = weight
 
-    edge = Edge(from_node, to_node, line['weight'])
-    edges[i] = edge
-    i += 1
+
+class Graph:
+    def __init__(self, file):
+        self.file = file
+        self.make_graph(file)
+
+    @staticmethod
+    def make_graph(file):
+        graph_file = open(file)
+        lines = json.load(graph_file)
+
+        nodes = dict()
+        edges = dict()
+
+        i = 1
+
+        for line in lines['edges']:
+            node1 = Node(line['node1'])
+            node2 = Node(line['node2'])
+            from_node = None
+            to_node = None
+
+            for node_key in nodes:
+                if nodes[node_key].node_number == node1.node_number:
+                    from_node = node_key
+                if nodes[node_key].node_number == node2.node_number:
+                    to_node = node_key
+
+            if not from_node:
+                from_node = node1
+                nodes_len = len(nodes)
+                nodes[nodes_len + 1] = from_node
+            if not to_node:
+                to_node = node2
+                nodes_len = len(nodes)
+                nodes[nodes_len + 1] = to_node
+
+            edge = Edge(from_node, to_node, line['weight'])
+            edges[i] = edge
+            i += 1
 
 # for node_key in nodes:
 #     print(nodes[node_key].node_number)
--- graph/Node.py
+++
@@ -1,3 +0,0 @@
-class Node:
-    def __init__(self, node_number):
-        self.node_number = node_number
