9460971c2cffdf685e2e470796b0be20cf245023
IsseNisse <isak.vindeln@gmail.com>
2021-08-06 01:34:55 +0200

    implemented heap permutation algorithm

--- algorithms/brute_force.py
+++ algorithms/brute_force.py
@@ -1,3 +1,5 @@
-from graph.Graph import Graph
+from graph.Graph import Graph, Node, Edge
 
 graph = Graph("../graph1.json")
+nodes = graph.get_nodes()
+nodes[1].heap_permutation(nodes, len(nodes))
--- graph/Graph.py
+++ graph/Graph.py
@@ -5,11 +5,33 @@ class Node:
     def __init__(self, node_number):
         self.node_number = node_number
 
+    def heap_permutation(self, a, size):
+        # print('size ' + str(size))
+        # if size becomes 1 then prints the obtained
+        # permutation
+        if size == 1:
+            for key in a:
+                print(a[key].node_number)
+            print(a)
+            return
+
+        for i in range(size):
+            self.heap_permutation(a, size - 1)
+
+            # if size is odd, swap 0th i.e (first)
+            # and (size-1)th i.e (last) element
+            # else If size is even, swap ith
+            # and (size-1)th i.e (last) element
+            if size & 1:
+                a[1], a[size] = a[size], a[1]
+            else:
+                a[i + 1], a[size] = a[size], a[i + 1]
+
 
 class Edge:
-    def __init__(self, nodeA, nodeB, weight):
-        self.node1 = nodeA
-        self.node2 = nodeB
+    def __init__(self, node_a, node_b, weight):
+        self.node1 = node_a
+        self.node2 = node_b
         self.weight = weight
 
 
@@ -18,8 +40,7 @@ class Graph:
         self.file = file
         self._make_graph(file)
 
-    @staticmethod
-    def _make_graph(file):
+    def _make_graph(self, file):
         try:
             graph_file = open(file)
         except FileNotFoundError:
@@ -27,7 +48,7 @@ class Graph:
 
         lines = json.load(graph_file)
 
-        nodes = dict()
+        self.nodes = dict()
         edges = dict()
 
         i = 1
@@ -38,24 +59,27 @@ class Graph:
             from_node = None
             to_node = None
 
-            for node_key in nodes:
-                if nodes[node_key].node_number == node1.node_number:
+            for node_key in self.nodes:
+                if self.nodes[node_key].node_number == node1.node_number:
                     from_node = node_key
-                if nodes[node_key].node_number == node2.node_number:
+                if self.nodes[node_key].node_number == node2.node_number:
                     to_node = node_key
 
             if not from_node:
                 from_node = node1
-                nodes_len = len(nodes)
-                nodes[nodes_len + 1] = from_node
+                nodes_len = len(self.nodes)
+                self.nodes[nodes_len + 1] = from_node
             if not to_node:
                 to_node = node2
-                nodes_len = len(nodes)
-                nodes[nodes_len + 1] = to_node
+                nodes_len = len(self.nodes)
+                self.nodes[nodes_len + 1] = to_node
 
             edge = Edge(from_node, to_node, line['weight'])
             edges[i] = edge
             i += 1
+
+    def get_nodes(self):
+        return self.nodes
 
 # for node_key in nodes:
 #     print(nodes[node_key].node_number)
