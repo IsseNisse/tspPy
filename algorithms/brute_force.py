f212877d3701c99c3bb86ed4ae2f5cccc7dbf5bc
IsseNisse <isak.vindeln@gmail.com>
2021-08-08 01:55:46 +0200

    added get_edge, add_edge and started on loops to go through all paths

--- algorithms/brute_force.py
+++ algorithms/brute_force.py
@@ -3,7 +3,17 @@ from graph.Graph import Graph, Node, Edge
 graph = Graph("../graph1.json")
 nodes = graph.get_nodes()
 heap_nodes = nodes.copy()
+first_node = heap_nodes[1]
+first_node_key = list(nodes.keys())[0]
 del heap_nodes[1]
-first_node = list(heap_nodes.keys())[0]
-all_paths = nodes[first_node].heap_permutation(heap_nodes, len(heap_nodes), first_node)
-print(all_paths)
+heap_first_node = list(heap_nodes.keys())[0]
+paths = nodes[heap_first_node].heap_permutation(heap_nodes, len(heap_nodes), heap_first_node)
+
+for path_key in paths:
+    path = paths[path_key]
+    path[first_node_key] = first_node
+    for node_key in path:
+        if node_key == len(path) - 1:
+            edge = path[node_key].get_edge(path[node_key + 1])
+            print(edge)
+
--- graph/Graph.py
+++ graph/Graph.py
@@ -5,6 +5,7 @@ class Node:
     def __init__(self, node_number):
         self.node_number = node_number
         self.path = dict()
+        self.edges = dict()
         self.i = 1
 
     def heap_permutation(self, a, size, first_node):
@@ -23,6 +24,19 @@ class Node:
             else:
                 a[i + first_node], a[size + first_node - 1] = a[size + first_node - 1], a[i + first_node]
         return self.path
+
+    def add_edge(self, edge, i):
+        self.edges[i] = edge
+
+    def get_edge(self, node):
+        edge_return = None
+        for edge_key in self.edges:
+            edge = self.edges[edge_key]
+            if edge.node1 == node:
+                edge_return = edge
+            elif edge.node2 == node:
+                edge_return = edge
+        return edge_return
 
 
 class Edge:
@@ -46,7 +60,7 @@ class Graph:
         lines = json.load(graph_file)
 
         self.nodes = dict()
-        edges = dict()
+        self.edges = dict()
 
         i = 1
 
@@ -58,9 +72,9 @@ class Graph:
 
             for node_key in self.nodes:
                 if self.nodes[node_key].node_number == node1.node_number:
-                    from_node = node_key
+                    from_node = self.nodes[node_key]
                 if self.nodes[node_key].node_number == node2.node_number:
-                    to_node = node_key
+                    to_node = self.nodes[node_key]
 
             if not from_node:
                 from_node = node1
@@ -72,11 +86,16 @@ class Graph:
                 self.nodes[nodes_len + 1] = to_node
 
             edge = Edge(from_node, to_node, line['weight'])
-            edges[i] = edge
+            from_node.add_edge(edge, i)
+            to_node.add_edge(edge, i)
+            self.edges[i] = edge
             i += 1
 
     def get_nodes(self):
         return self.nodes
+
+    def get_edges(self):
+        return self.edges
 
 # for node_key in nodes:
 #     print(nodes[node_key].node_number)
