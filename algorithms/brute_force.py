8d8c9bd9a691f534f54a737e9f213d2e858be3a5
IsseNisse <isak.vindeln@gmail.com>
2021-08-09 01:52:51 +0200

    have now worked out the bugs found in brute force algorithm

--- algorithms/brute_force.py
+++ algorithms/brute_force.py
@@ -16,24 +16,28 @@ for path_key in paths:
     path[first_node_key] = first_node
     edges = []
     total_w = 0
-    for node_key in path:
-        if node_key < len(path) - 1:
-            edge = path[node_key].get_edge(path[node_key + 1])
-            edges.append(edge)
-        else:
+    for node_key in range(1, len(path) + 1):
+
+        if node_key == len(path):
             edge = path[node_key].get_edge(path[first_node_key])
             edges.append(edge)
+        else:
+            edge = path[node_key].get_edge(path[node_key + 1])
+            edges.append(edge)
+
     for edge in edges:
         total_w += edge.weight
-        print(edge.weight)
 
     print('total: ' + str(total_w) + '\n')
+    if shortest_weight == total_w:
+        alt_path = path
 
     if shortest_weight == 0 or total_w < shortest_weight:
         shortest_weight = total_w
         shortest_path = path
 
 
-# print(shortest_weight)
+print(shortest_weight)
 print(str(shortest_path[1].node_number) + '' + str(shortest_path[2].node_number) + '' + str(shortest_path[3].node_number) + '' + str(shortest_path[4].node_number) + '' + str(shortest_path[5].node_number))
+print('alt path: ' + str(alt_path[1].node_number) + '' + str(alt_path[2].node_number) + '' + str(alt_path[3].node_number) + '' + str(alt_path[4].node_number) + '' + str(alt_path[5].node_number))
 
